6eadad0e166531332034cbf0c8470716e59e4471
Mason <SOROM2@student.op.ac.nz>
2020-10-20 00:45:03 +1300

    add random functions to rt funcs

--- src/rtweekend.h
+++ src/rtweekend.h
@@ -2,6 +2,7 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <random>
 #include <limits>
 #include <memory>
 
@@ -11,6 +12,16 @@ const float pi = 3.1415926535897932385f;
 inline float degreesToRadians(float degrees)
 {
     return degrees * pi / 180.0f;
+}
+
+inline float randomFloat() {
+    static std::uniform_real_distribution<float> distribution(0.0f, 1.0f);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline float randomFloat(float min, float max) {
+    return min + (max - min) * randomFloat();
 }
 
 #include "ray.h"
