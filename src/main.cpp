5158a02ccf0dc6ad9938459505f0cec45e636322
Mason <SOROM2@student.op.ac.nz>
2020-10-19 18:51:47 +1300

    Use vec3 in main

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <string>
 
+#include "vec3.h"
+
 /**
  * takes path and content strings,
  * writes out to a file if possible.
@@ -40,13 +42,11 @@ int main(int argc, char* argv[])
 
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2f;
+            Vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2f);
 
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
+            int ir = int(255.99 * col[0]);
+            int ig = int(255.99 * col[1]);
+            int ib = int(255.99 * col[2]);
 
             contentStream << ir << " " << ig << " " << ib << "\n";
         }
